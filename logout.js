e540fcb217d720679818e7bcf101eb0a9c6679a0
alfariskhan05 <sec23it118@sairamtap.edu.in>
2025-05-14 20:38:58 +0530

    Delete logout.js

--- logout.js
+++
@@ -1,8 +0,0 @@
-// logout.js
-function logout() {
-    // Clear session data (or cookies if used)
-    sessionStorage.clear();  // Or cookies if you're using them
-
-    // Redirect to the login page
-    window.location.href = "login.html";
-}
